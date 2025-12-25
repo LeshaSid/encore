7087fd18b8c425212c297db9cae5c2609e9acf47
qawdesm <n.grigorenko2005@gmail.com>
2025-12-26 00:00:38 +0300

    delete

--- update_authorization/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class UpdateAuthorizationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'update_authorization'
-    verbose_name = 'Обновленная авторизация'
