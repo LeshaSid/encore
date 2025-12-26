4537c1905339f8f4d6c2f4533f34d632789c6c15
mwatakazembe <kill.va.udaki@gmail.com>
2025-12-26 06:27:48 +0300

    cm_tests

--- concertsshower/tests.py
+++ concertsshower/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
-from core.models import Concert
+from django.contrib.auth.models import User
 from django.utils import timezone
+from .models import Concert
+from .utils import get_user_role
 
 class ConcertViewTests(TestCase):
     def setUp(self):
@@ -22,3 +24,62 @@ class ConcertViewTests(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Minsk, Arena")
+
+class RoleTests(TestCase):
+    def setUp(self):
+        self.organizer = User.objects.create_user(
+            username="organizer",
+            password="testpass",
+            is_staff=True
+        )
+        self.musician_user = User.objects.create_user(
+            username="musician",
+            password="testpass"
+        )
+        self.viewer_user = User.objects.create_user(
+            username="viewer", 
+            password="testpass"
+        )
+
+    def test_get_user_role_organizer(self):
+        role = get_user_role(self.organizer)
+        self.assertEqual(role, 'organizer')
+
+    def test_get_user_role_musician(self):
+        role = get_user_role(self.musician_user)
+        self.assertEqual(role, 'viewer')
+
+class AccessTests(TestCase):
+    def setUp(self):
+        self.organizer = User.objects.create_user(
+            username="organizer",
+            password="testpass",
+            is_staff=True
+        )
+        self.musician = User.objects.create_user(
+            username="musician",
+            password="testpass"
+        )
+        self.concert = Concert.objects.create(
+            concert_title="Test Concert",
+            venue_address="Test Address",
+            concert_date=timezone.now() + timezone.timedelta(days=1)
+        )
+
+    def test_all_concerts_access_organizer(self):
+        self.client.login(username='organizer', password='testpass')
+        url = reverse('concertsshower:all_concerts')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_all_concerts_access_musician(self):
+        self.client.login(username='musician', password='testpass')
+        url = reverse('concertsshower:all_concerts')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_add_concert_access_organizer(self):
+        self.client.login(username='organizer', password='testpass')
+        url = reverse('concertsshower:add_concert')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
